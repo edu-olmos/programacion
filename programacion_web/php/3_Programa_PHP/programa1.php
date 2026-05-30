40e4d0eb61a01c0f9f4bc9988572479525259bc9
Ing Eduardo <edujosepeolms@gmail.com>
2026-05-30 17:17:41 -0400

    php

---
+++ programacion_web/php/3_Programa_PHP/programa1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+
+  <?php
+  echo "Mi nombre es Eduardo.";
+  echo "Tengo 38 años.";
+  ?>
+
+</body>
---
+++ programacion_web/php/5_Tipos_de_variables/problema.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+
+  <?php
+
+  $edad = 38;
+  $peso = 86.25;
+  $nombre = "Eduardo";
+  $existe = true;
+  echo "Variable integer:";
+  echo $edad;
+  echo "<br>";
+  echo "Variable double:";
+  echo $peso;
+  echo "<br>";
+  echo "Variable string:";
+  echo $nombre;
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $existe;
+
+  ?>
+
+</body>
+
+</html>
