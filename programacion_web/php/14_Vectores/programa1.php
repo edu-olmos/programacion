0b3f5ccd85467797ea046a9f370c93bfd00a3f8f
Ing Eduardo <edujosepeolms@gmail.com>
2026-06-10 11:02:55 -0400

    php

---
+++ programacion_web/php/14_Vectores/programa1.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+
+  <?php
+  $nombres[] = "juan";
+  $nombres[] = "pedro";
+  $nombres[] = "ana";
+  foreach ($nombres as $nombre) {
+    echo $nombre;
+    echo "<br>";
+  }
+  ?>
+
+</body>
+
+</html>
