eefdd5baf07aa199ead3c5fa7e492978b6ec512c
Ing Eduardo <edujosepeolms@gmail.com>
2026-06-16 12:07:12 -0400

    php

---
+++ programacion_web/php/24_delete_all/problema.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  $conexion = mysqli_connect("localhost", "eduardo", "4321", "base1") or
+    die("Problemas con la conexión");
+
+  mysqli_query($conexion, "delete from cursos") or
+    die("Problemas en el select:" . mysqli_error($conexion));
+  echo "Se efectuó el borrado de todos los cursos.";
+  mysqli_close($conexion);
+  ?>
+</body>
+
+</html>
---
+++ programacion_web/php/25_UPDATE/problema2.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+    <?php
+    $conexion = mysqli_connect("localhost", "eduardo", "4321", "base1") or
+        die("Problemas con la conexión");
+
+    mysqli_query($conexion, "update cursos
+                          set nombrecurso='$_REQUEST[nombrecurso]' 
+                        where codigo=$_REQUEST[codigo]") or
+        die("Problemas en el select:" . mysqli_error($conexion));
+    echo "Se modificó el nombre del curso";
+    ?>
+</body>
+
+</html>
